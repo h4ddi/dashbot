29a2b49d2a0550f77c01ad27760a8b6cdfffed8c
petrspelos <petr.spelos@gmail.com>
2019-02-27 17:38:59 +0100

    Remove commented out code

--- JsonStorage/JsonPersistentStorage.cs
+++ JsonStorage/JsonPersistentStorage.cs
@@ -15,45 +15,6 @@ namespace DashBot.DataStorage
         public static string ToStoragePath(Type objType, string collection)
             => Path.Combine(JsonDataDirectory, objType.Name, collection);
 
-        //public T Restore<T>(string path)
-        //{
-        //var fullPath = GetStoragePathForFile(typeof(T), path);
-        //return RestoreFromFile<T>(fullPath);
-        //}
-        //
-        //public IEnumerable<T> RestoreCollection<T>(string path)
-        //{
-        //var collectionPath = GetPathForCollection(typeof(T), path);
-        //Assert.DirectoryExists(collectionPath);
-        //var files = Directory.GetFiles(collectionPath, "*.json");
-        //return files.Select(f => RestoreFromFile<T>(f));
-        //}
-        //
-        //public void Store<T>(T obj, string path)
-        //{
-        //Assert.NotEnumerable(typeof(T));
-        //var fullPath = GetStoragePathForFile(typeof(T), path);
-        //}
-        //
-        //private T RestoreFromFile<T>(string filePath)
-        //{
-        //Assert.FileExists(filePath);
-        //var json = File.ReadAllText(filePath);
-        //return JsonConvert.DeserializeObject<T>(json);
-        //}
-        //
-        //public void Store<T>(T obj, string collection, string key)
-        //{
-        //var filePath = ToStoragePath(typeof(T), collection, key);
-        //var json = JsonConvert.SerializeObject(obj);
-        //Directory.CreateDirectory(Path.GetDirectoryName(filePath));
-        //File.WriteAllText(filePath, json);
-        //}
-        //
-        //public IEnumerable<T> RestoreCollection(string collection, string pattern)
-        //{
-        //throw new NotImplementedException();
-        //}
         public void Store<T>(T obj, string collection, string key)
         {
             var path = ToStoragePath(typeof(T), collection);
@@ -83,3 +44,4 @@ namespace DashBot.DataStorage
         }
     }
 }
+
