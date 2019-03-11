c857c8d9b1f21b9dbc4cc49841fe4c3af4dbc15e
petrspelos <petr.spelos@gmail.com>
2019-03-11 10:15:08 +0100

    Refactor signalR javascript

---
+++ Server/wwwroot/js/botStatusWidget.js
@@ -0,0 +1,46 @@
+﻿var $statusActionBtn = $("#StatusActionButton");
+var $statusMessage = $("#StatusMessage");
+var $statusIcon = $("#StatusIcon");
+var $statusWidget = $("#StatusWidget");
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/botHub").build();
+
+connection.start().then(function () {
+    // on connection started
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+connection.on("BotConnectedChanged", setBotConnected);
+
+function setBotConnected(isConnected) {
+    $statusActionBtn.removeAttr("disabled");
+
+    if (isConnected) {
+        $statusActionBtn.text("Stop");
+        $statusMessage.text("Bot is running...");
+        overrideClass($statusActionBtn, "btn-outline-success", "btn-outline-secondary");
+        overrideClass($statusIcon, "fa-pause", "fa-play");
+        overrideClass($statusWidget, "status-negative", "status-positive");
+    } else {
+        $statusActionBtn.text("Start");
+        $statusMessage.text("Bot is not running.");
+        overrideClass($statusActionBtn, "btn-outline-secondary", "btn-outline-success");
+        overrideClass($statusIcon, "fa-play", "fa-pause");
+        overrideClass($statusWidget, "status-positive", "status-negative");
+    }
+}
+
+$statusActionBtn.click(() => {
+    $statusActionBtn.attr("disabled", "disabled");
+    $statusMessage.text("Changing status...");
+
+    connection.invoke("ToggleBotConnection").catch((err) => {
+        return console.error(err.toString());
+    });
+});
+
+function overrideClass($element, classA, classB) {
+    $element.removeClass(classA);
+    $element.addClass(classB);
+}
